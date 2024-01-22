6237ed2bc672769bc257a93357c07e19244fd0b1
Sebastiand22 <sebastiandunduc16@gmail.com>
2024-01-22 18:56:03 +0200

    first

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import styles from '../Header/Header.module.css'
+
+const Header = () => {
+
+    return ( 
+        <div className={styles.header}>
+            <h1>Guardian Funerals </h1>
+            <ul>
+<li>About Us</li>
+<li>Pricing</li>
+<li>Shop</li>
+<li>Cars</li>
+<li>Team</li>
+
+            </ul>
+             </div>
+    )
+}
+
+
+
+export default Header;
